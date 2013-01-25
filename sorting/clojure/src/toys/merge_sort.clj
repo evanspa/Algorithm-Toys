d2c7e41eacb961556cca3fac37ea004c24554229
Paul Evans <evansp2@gmail.com>
2013-01-25 01:14:13 -0500

    completed clojure merge sort

--- sorting/clojure/src/toys/merge_sort.clj
+++ sorting/clojure/src/toys/merge_sort.clj
@@ -3,80 +3,68 @@
       :author "Paul Evans"}
     toys.merge-sort)
 
-(defn merge-sort-tramp
-  "Sorts the given list using merge sort algorithm."
-  [l]
-  (if (or (nil? l)
-          (= (count l) 0)
-          (= (count l) 1))
-    l
-    (letfn [(merge-cps [left right k]
-              (let [left-count (count left)
-                    right-count (count right)
-                    left-first (first left)
-                    right-first (first right)]
-                (if (or (> left-count 0)
-                        (> right-count 0))
-                  (if (and (> left-count 0)
-                           (> right-count 0))
-                    (if (<= left-first right-first)
-                      (trampoline
-                       #(merge-cps
-                         (rest left)
-                         right
-                         (fn [v] (conj v left-first))))
-                      (trampoline
-                       #(merge-cps
-                         left
-                         (rest right)
-                         (fn [v] (conj v right-first))))))
+(defn merge-recursive
+  "Implementation of merge that yields a recursive process.  I.e., this version
+cannot leverage recur because the resursive call is not in the tail position.
+FYI, in the SICP lectures, Sussman contrasts 2 different implementations of the
+same function; one was defined with an iterative strategy; the other with a
+recursive strategy.  The same is true here.  This version happens to be the
+recursive strategy."
+  [left right]
+  (let [left-count (count left)
+        right-count (count right)
+        left-first (first left)
+        right-first (first right)]
+    (if (and (= left-count 0)
+             (= right-count 0))
+      []
+      (if (and (> left-count 0)
+               (> right-count 0))
+        (if (<= left-first right-first)
+          (cons left-first (merge-recursive (rest left) right))
+          (cons right-first (merge-recursive left (rest right))))
+        (if (> left-count 0)
+          (cons left-first (merge-recursive (rest left) right))
+          (cons right-first (merge-recursive left (rest right))))))))
+
+(defn merge-iterative
+  "Implementation of merge that yeilds an iterative process.  I.e., all the
+information necessary to carry out the computation is parameterized within
+the internal merge-iterative-internal procedure. Because of this, the recursive
+calls are naturally in the tail position, and thus allows the usage of recur."
+  [left right]
+  (letfn [(merge-iterative-internal [left right result]
+            (let [left-count (count left)
+                  right-count (count right)
+                  left-first (first left)
+                  right-first (first right)]
+              (if (and (= left-count 0)
+                       (= right-count 0))
+                result
+                (if (and (> left-count 0)
+                         (> right-count 0))
+                  (if (<= left-first right-first)
+                    (recur (rest left) right (conj result left-first))
+                    (recur left (rest right) (conj result right-first)))
                   (if (> left-count 0)
-                    (trampoline
-                     #(merge-cps
-                       (rest left)
-                       right
-                       (fn [v] (conj v left-first))))
-                    (when (> right-count 0)
-                      (trampoline
-                       #(merge-cps
-                         left
-                         (rest right)
-                         (fn [v] (conj v right-first)))))))
-                (k (vec '()))))]
-      (let [count-halved (int (/ (count l) 2))
-            left (take count-halved l)
-            right (nthrest l count-halved)]
-        (merge-cps (trampoline #(merge-sort left))
-                   (trampoline #(merge-sort right))
-                   (fn [v] v))))))
+                    (recur (rest left) right (conj result left-first))
+                    (recur left (rest right) (conj result right-first)))))))]
+    (merge-iterative-internal left right [])))
 
 (defn merge-sort
-  "Sorts the given list using merge sort algorithm."
+  "Sorts the given list using merge sort algorithm.  Because merge-serge is
+doubly recusive, it is not possible to yield a purely iterative process; it
+is not possible to use recur.  Therefore, in order to prevent stack growth,
+the trampoline technique is leveraged."
   [l]
-  (if (or (nil? l)
-          (= (count l) 0)
-          (= (count l) 1))
-    l
-    (letfn [(merge [left right result]
-              (let [left-count (count left)
-                    right-count (count right)
-                    left-first (first left)
-                    right-first (first right)]
-                (if (or (> left-count 0)
-                        (> right-count 0))
-                  (if (and (> left-count 0)
-                           (> right-count 0))
-                    (if (<= left-first right-first)
-                      (merge (rest left) right (conj result left-first))
-                      (merge left (rest right) (conj result right-first))))
-                  (if (> left-count 0)
-                    (merge (rest left) right (conj result left-first))
-                    (when (> right-count 0)
-                      (merge left (rest right) (conj result right-first)))))
-                result))]
-      (let [count-halved (int (/ (count l) 2))
-            left (take count-halved l)
-            right (nthrest l count-halved)]
-        (merge (merge-sort left)
-               (merge-sort right)
-               (vector))))))
+  (letfn [(merge-sort-tramp [l]
+            (if (or (nil? l)
+                    (= (count l) 0)
+                    (= (count l) 1))
+              l
+              (let [count-halved (int (/ (count l) 2))
+                    left (take count-halved l)
+                    right (nthrest l count-halved)]
+                (merge-iterative (trampoline #(merge-sort-tramp left))
+                                 (trampoline #(merge-sort-tramp right))))))]
+    (trampoline #(merge-sort-tramp l))))
--- sorting/clojure/test/toys/merge_sort_test.clj
+++ sorting/clojure/test/toys/merge_sort_test.clj
@@ -5,4 +5,5 @@
   (:use [toys.merge-sort :only [merge-sort]]))
 
 (deftest test-merge-sort
-  (is (= '(a b c) (merge-sort '(b c a)))))
+  (is (= '(1 2 3) (merge-sort '(3 1 2))))
+  (is (= '(9 10 11 12 13) (merge-sort '(12 13 9 11 10)))))
