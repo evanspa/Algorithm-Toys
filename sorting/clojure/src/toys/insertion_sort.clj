e538f7e60a4688de822d970506b7592fc7f66126
Paul Evans <evansp2@gmail.com>
2013-02-16 21:44:24 -0500

    added insertion sort for Clojure

--- sorting/clojure/src/toys/insertion_sort.clj
+++ sorting/clojure/src/toys/insertion_sort.clj
@@ -1,7 +1,37 @@
 (ns
-    ^{:doc "Implementation of insertion algorithm."
+    ^{:doc "Implementation of insertion sort algorithm."
       :author "Paul Evans"}
     toys.insertion-sort)
 
-(defn insertion-sort [list]
-  )
+(defn insert
+  "Inserts value into sorted list."
+  [value list]
+  (loop [list list
+         newlist []]
+    (if (empty? list)
+      (vec (cons value newlist))
+      (if (= (count list) 1)
+        (if (< value (first list))
+          (vec (cons value (vec (cons (first list) newlist))))
+          (vec (conj (vec (cons (first list) newlist)) value)))
+        (if (and (<= value (last list))
+                 (> value (nth list (- (count list) 2))))
+          (vec (concat (take (dec (count list)) list)
+                       (cons value (cons (last list) newlist))))
+          (if (> value (last list))
+            (vec (conj list value))
+            (recur (take (dec (count list)) list)
+                   (cons (last list) newlist))))))))
+
+(defn insertion-sort
+  "Sorts list using insertion sort algorithm."
+  [list]
+  (if (or (empty? list)
+          (= (count list) 1))
+      list
+      (loop [sorted-list (vector (first list))
+             index 1]
+        (if (= (count sorted-list) (count list))
+          sorted-list
+          (let [val (nth list index)]
+            (recur (insert val sorted-list) (inc index)))))))
